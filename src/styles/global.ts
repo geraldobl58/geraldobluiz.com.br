125a0da7891bdea582f09407b5988e5d9a8942f1
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-04 17:28:26 -0300

    :art: Using Web Fonts in Next

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,86 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 100;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-100.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 200;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-200.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-300.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-regular.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-500.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-600.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-700.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 800;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-800.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 900;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v18-latin-900.woff2') format('woff2'),
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothig: grayscale;
   }
 
   html {
@@ -16,7 +92,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    font-family: 'Montserrat', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
