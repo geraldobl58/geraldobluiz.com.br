a5a0c39e2025a2d6bddded267c4ce227cfef650c
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-08 14:19:16 -0300

    :sparkles: :rotating_light: Creating a renderWithTheme test helper and Creating first tests

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,9 +1,41 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
+const wrapperModifiers = {
+  normal: () => css`
+    width: 18rem;
+    height: 3rem;
+  `,
+
+  large: () => css`
+    width: 21rem;
+    height: 5rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
+
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,49 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Geraldo Luiz/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a yellow label when color is passed', () => {
+    renderWithTheme(<Logo color="secondary" />)
+
+    expect(screen.getByLabelText(/Geraldo Luiz/i).parentElement).toHaveStyle({
+      color: '#FF9901'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Geraldo Luiz/i).parentElement).toHaveStyle({
+      width: '21rem'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Geraldo Luiz/i).parentElement).toHaveStyle({
+      width: '18rem'
+    })
+  })
+
+  it('should render a bigger logo without text on hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(
+      screen.getByLabelText(/Geraldo Luiz/i).parentElement
+    ).toHaveStyleRule('width', '5.8rem', {
+      media: '(max-width: 768px)'
+    })
   })
 })
