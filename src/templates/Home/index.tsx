a447ab4012a9966ec2ab1d2bc0716964d8dedf88
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-10 09:05:00 -0300

    :sparkles: :rotating_light: Adding the first static components and tests

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,36 @@
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+import Menu from 'components/Menu'
+import SubTitle from 'components/SubTitle'
+
 import * as S from './styles'
 
 const Home = () => (
-  <S.Wrapper>
-    <h1>Home</h1>
-  </S.Wrapper>
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <SubTitle>Visite meu portfolio</SubTitle>
+      <Heading lineLeft>Meu portfolio</Heading>
+    </Container>
+
+    <Container>
+      <SubTitle>Tecnologias</SubTitle>
+      <Heading lineLeft>Algumas tecnologias usadas</Heading>
+    </Container>
+
+    <Container>
+      <SubTitle>Blog</SubTitle>
+      <Heading lineLeft>Confira algumas dicas</Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
 )
 
 export default Home
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,13 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Home from '.'
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Home />)
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(/© 2021.Todos os direitos reservados./i)
+    ).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the sections', () => {
+    renderWithTheme(<Home />)
+
+    expect(
+      screen.getByRole('heading', { name: /visite meu portfolio/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /algumas tecnologias usadas/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /confira algumas dicas/i })
+    ).toBeInTheDocument()
   })
 })
