b457889bab7c7ac0a08bed12e7a174ac2e8285d9
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-13 14:23:46 -0300

    :sparkles: Mocking the data

--- src/components/Card/stories.tsx
+++ src/components/Card/stories.tsx
@@ -1,15 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Card, { CardProps } from '.'
 
+import item from './mock'
+
 export default {
   title: 'Card',
   component: Card,
-  args: {
-    subtitle: 'Desenvolvimento',
-    title: 'Mobile app landing design & app maintain',
-    img:
-      'https://images.unsplash.com/photo-1514575110897-1253ff7b2ccb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=380&q=80'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<CardProps> = (args) => (
