1a0b9012bf56662222aa55ef86fbf75fabb276c6
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-21 16:20:01 -0300

    :sparkles: NextJS and writing queries

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,3 +1,6 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+
 import { Container } from 'components/Container'
 import Heading from 'components/Heading'
 import SubTitle from 'components/SubTitle'
@@ -5,14 +8,24 @@ import SubTitle from 'components/SubTitle'
 import About from 'templates/About'
 import Base from 'templates/Base'
 
-const Me = () => (
-  <Base>
-    <Container>
-      <SubTitle>Sobre</SubTitle>
-      <Heading lineLeft>Um pouco sobre mim</Heading>
-      <About />
-    </Container>
-  </Base>
-)
+export default function Me() {
+  return (
+    <Base>
+      <Container>
+        <SubTitle>Sobre</SubTitle>
+        <Heading lineLeft>Um pouco sobre mim</Heading>
+        <About />
+      </Container>
+    </Base>
+  )
+}
 
-export default Me
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
