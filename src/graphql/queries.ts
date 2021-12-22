eb4b4fd92ef84b938cca66a916ff86e75c30a913
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-22 09:29:10 -0300

    :sparkles: Creating queries to assemble api data

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,11 +1,26 @@
 import { gql } from 'graphql-request'
 
 export const GET_PAGES = gql`
-  query getPages {
-    pages {
+  query getPages($first: Int) {
+    pages(first: $first) {
       id
       heading
+      subheading
       slug
+      body {
+        html
+      }
+    }
+  }
+`
+
+export const GET_PAGE_BY_SLUG = gql`
+  query getPageBySlug($slug: String!) {
+    page(where: { slug: $slug }) {
+      id
+      slug
+      heading
+      subheading
       body {
         html
       }
