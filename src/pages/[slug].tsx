fe05c8eba251e4f69c2407264381a61f9d3e19fc
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-22 10:21:52 -0300

    :sparkles: Generating types for GraphQL using GraphQL Codegen

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import client from 'graphql/client'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
 
 import { Container } from 'components/Container'
 
@@ -23,7 +24,7 @@ export default function Page({ heading, subheading, body }: PageTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -33,7 +34,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
