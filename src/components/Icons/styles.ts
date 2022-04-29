1d7ab8fedfe489c57c85353ac226abf63fc90686
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-29 15:43:27 -0300

    :hammer: Fixing icons

--- src/components/Icons/styles.ts
+++ src/components/Icons/styles.ts
@@ -10,28 +10,25 @@ export const Wrapper = styled.main`
 
 export const Column = styled.div`
   ${({ theme }) => css`
-    width: 10rem;
-    height: 10rem;
+    width: 5rem;
+    height: 5rem;
     background: ${theme.colors.gray};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 9px;
+    padding: 10px;
+
     display: flex;
     align-items: center;
-    justify-content: center;
-    margin: 0 auto;
+    justify-content: space-between;
+
+    img {
+      width: 100%;
+    }
   `}
 `
 
 export const Content = styled.div`
-  ${({ theme }) => css`
-    display: grid;
-    grid-template-columns: minmax(auto, 50%) 1fr;
-    gap: ${theme.grid.gutter};
-    margin-top: ${theme.spacings.medium};
-    align-items: center;
-
-    ${media.greaterThan('medium')`
-      grid-template-columns: repeat(5, 1fr)
-    `}
-  `}
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
